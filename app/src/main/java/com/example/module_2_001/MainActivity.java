ca05be6db3cd76069abfed20cdea111cf15cb0db
Rafael Hidalgo <hidalgor2@montclair.edu>
2022-05-25 17:06:03 -0400

    Fourth Commit

--- app/src/main/java/com/example/module_2_001/MainActivity.java
+++ app/src/main/java/com/example/module_2_001/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.module_2_001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView myTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myTextView=(TextView)findViewById(R.id.textView2);
+    }
+
+    public void doSomething(View view) {
     }
 }
